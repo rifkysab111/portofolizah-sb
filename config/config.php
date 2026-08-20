5edbd94561fa090a560306d7cef18f39181e1379
Moch. Rifky Sabilul Khaqiq <23424002.student@unusida.ac.id>
2026-08-20 09:04:33 +0700

    Fix PDO emulate prepares untuk Supabase Pooler

--- config/config.php
+++ config/config.php
@@ -27,7 +27,7 @@ $dsn = "pgsql:host={$host};port={$port};dbname={$dbname};sslmode=require";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES   => true,
 ];
 
 try {
